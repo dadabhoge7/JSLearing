058974573eda9180e9714ca7536ed27051a4cb05
dadabhoge7 <dadabhoge7@gmail.com>
2023-09-25 10:46:38 +0530

    Add function 03_assignment02
    Add function 03_assignment02

---
+++ 03_funAssign02.js
@@ -0,0 +1,6 @@
+function bankDetails(bankName, accountNum, location, pinCode){
+console.log("BankName : ",bankName,",","AccountNumber : " ,accountNum,",","BankLocation : " ,location,",", "PinCode :",pinCode);
+}
+bankDetails("CitiBank",3456782345, 'Pune', 432203);
+bankDetails("AxixBank",7856782345, 'Mumbai', 441202);
+bankDetails("HDFCBank",8956782345, 'Pune', 611202);
