377d38c9ebc26c4ec4ac45e9bf4505301f83efee
dadabhoge7 <dadabhoge7@gmail.com>
2023-09-27 00:49:52 +0530

    string template assignment
    string template assignment

---
+++ 03_stringTemplateAssign.js
@@ -0,0 +1,12 @@
+function stringTemplateAssign()
+{
+    console.log(`My dream company is "Google"`);
+    var hobby1 = "Reading";
+    var hobby2 = "Programming";
+    var hobby3 = "Trekking";
+    console.log(`2.1 my Hobby` ,hobby1, hobby2, hobby3);
+    console.log(`2.2 Hobby 1 is ${hobby1} , Hobby 2 is ${hobby2} ,Hobby 3 is ${hobby3}`);
+    console.log("length of hobby1 : ",hobby1.length,"length of hobby2 : ",hobby2.length,"length of hobby3 : ",hobby3.length);
+    console.log("addition of length : ",hobby1.length+hobby2.length+hobby3.length);
+}
+stringTemplateAssign()
