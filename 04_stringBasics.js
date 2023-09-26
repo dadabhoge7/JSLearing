9e39f6ef3b1cbbdd549fd7f16c26d568d9b35e86
dadabhoge7 <dadabhoge7@gmail.com>
2023-09-27 00:35:37 +0530

    add string basic
    add string basic

--- 04_stringBasics.js
+++ 04_stringBasics.js
@@ -52,9 +52,52 @@ var city="My City is Pune        ";
 console.log("Before trim City : ",city.length);
 var afterTrim=city.trimEnd();
 console.log("After trim City : ",afterTrim.length);
+// Using string tamplate
+console.log(`After trim city : ${afterTrim.length}`);
 
+console.log("================concat() Method========================");
+var str1="Dada";
+var str2="Bhoge";
+console.log(str1.concat(str2));
 
+console.log("================includes() Method========================");
+var result=greet.includes('Morning');
+console.log("Is Morning word available : ",result);
+var result=greet.includes('morning');
+console.log("Is morning word available : ",result);
 
+console.log("================search() Method========================");
+var result=greet.search('or');
+console.log(result);
+var result=greet.search('oo');
+console.log(result);
+
+console.log("================slice() Method========================");
+var treet="Hello, Good Morning";
+console.log(treet.slice(8));
+console.log(treet.slice(8, 14));
+
+console.log("================split() Method========================");
+var treet="Hello, Good Morning";
+console.log(treet.split(" "));
+
+// WAP to count the total number of words
+var str= "i am angular developer";
+var words=str.split(" ");
+console.log(words);
+console.log(typeof words);
+// Using string tamplate
+console.log(`${typeof words}`);
+console.log('Total words is : ',words.length)
+// Using string tamplate
+console.log(`Total words is : ${words.length}`);
+
+// string template  `bactich`
+console.log(`i am "UI" developer`);
+var a=10;
+var b=20;
+var c=a+b;
+console.log(` Addition of ${a} and ${b} is : ${c}`);
 
 
 
