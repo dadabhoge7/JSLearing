71b384fdd9cda7aa0262e5d587600bef156db00b
dadabhoge7 <dadabhoge7@gmail.com>
2023-09-28 09:44:21 +0530

    add 05_NAN (Nota Number)
    add 05_NAN (Nota Number)

---
+++ 05_nan.js
@@ -0,0 +1,15 @@
+var num1=0;
+var num2=0;
+var result=num1/num2;
+console.log(result);
+console.log(typeof result);
+
+var num="20";
+console.log(typeof num);
+var convert=+num;
+console.log(typeof convert);
+
+var num2="Dada";
+console.log(typeof num2);
+var convert=+num2;
+console.log(typeof convert);
