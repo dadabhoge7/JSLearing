ac0bdad72ffdb9ff99d02a314b585490ca83719c
dadabhoge7 <dadabhoge7@gmail.com>
2023-09-28 11:38:59 +0530

    update 05_NaN
    update 05_NaN

--- 05_nan.js
+++ 05_nan.js
@@ -2,14 +2,13 @@ var num1=0;
 var num2=0;
 var result=num1/num2;
 console.log(result);
-console.log(typeof result);
+console.log(    typeof result);
 
 var num="20";
-console.log(typeof num);
+console.log(`String "${num}" Type Of : ${typeof num}`);
 var convert=+num;
-console.log(typeof convert);
+console.log(`convert "${num}" to number and Type Of: ${typeof convert}`);
 
-var num2="Dada";
-console.log(typeof num2);
-var convert=+num2;
-console.log(typeof convert);
+var num1="Dada";
+var result=+num1;
+console.log(result, typeof result); 
