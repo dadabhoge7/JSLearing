40a87ed207835e48771833c6f00c1aa6f8abbb48
dadabhoge7 <dadabhoge7@gmail.com>
2023-09-30 00:46:46 +0530

    add 03_Function expression
    add 03_Function expression

---
+++ 03_functionExpression.js
@@ -0,0 +1,15 @@
+var show = function(){
+    console.log(`Inside Function Expresstion....`);
+}
+show();
+
+var add = function(Value1, value2) {
+    console.log(Value1+value2);
+}
+add(20, 10);
+
+var squre = function (value) {
+    return value * value
+}
+var result = squre(25);
+console.log(result);
