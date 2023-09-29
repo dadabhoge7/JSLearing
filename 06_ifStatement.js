4e529f6adf93fec4d9441788480271732052b693
dadabhoge7 <dadabhoge7@gmail.com>
2023-09-30 00:48:05 +0530

    add 06_ifStatement
    add 06_ifStatement

---
+++ 06_ifStatement.js
@@ -0,0 +1,15 @@
+var girlAge = 19;
+if(girlAge>=18){
+    console.log(`You could go inside office`);
+    console.log(`Have you informed your parent`);
+    console.log(`Signed.....congrats!!!`);
+}
+console.log(`Daily Life`);
+
+console.log(`==============================`);
+var age=10;
+if (age>=18) {
+    console.log(` age ${age} Allow for Voting`);
+} else {
+    console.log(`Age ${age} Dont allow for Voting`);
+}
