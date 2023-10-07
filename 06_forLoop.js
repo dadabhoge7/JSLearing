73c85cc4c31560f362a565d15fc7a50ae8b5024e
dadabhoge7 <dadabhoge7@gmail.com>
2023-10-07 09:44:59 +0530

    add 06_forLoop
    add 06_forLoop

---
+++ 06_forLoop.js
@@ -0,0 +1,14 @@
+for (let index = 0; index <= 20; index++) {
+console.log(index);
+}
+for (let index = 0; index <= 10; index=index+2) {
+    console.log(index);
+}
+for (let index = 5; index <= 50; index=index+5) {
+  console.log(index);
+}
+
+for (let index = 10; index >= 0; index++) {
+   console.log(index);
+    
+}
