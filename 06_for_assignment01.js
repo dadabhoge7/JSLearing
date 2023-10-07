9c4883f226d4f706dc923e7f535d1b6a1206fe0c
dadabhoge7 <dadabhoge7@gmail.com>
2023-10-07 09:44:37 +0530

    add 06_for_assignmenet01
    add 06_for_assignmenet01

---
+++ 06_for_assignment01.js
@@ -0,0 +1,44 @@
+console.log(`============Step 01=============`);
+for (let index = 5; index <= 15; index++) {
+   console.log(index);
+    
+}
+console.log(`============Step 02=============`);
+
+for (let index = 55; index >= 40; index--) {
+   console.log(index);
+    
+}
+console.log(`============Step 03=============`);
+
+for (let index = 1; index <=30; index=index+2) {
+    console.log(index);
+    
+}
+console.log(`============Step 04=============`);
+
+for (let index = 2; index <= 20; index=index+2) {
+   console.log(index);
+    
+}
+console.log(`============Step 05=============`);
+
+for (let index = 5; index <= 50; index=index+5) {
+    console.log(index);
+    
+}
+console.log(`============Step 06=============`);
+
+for (let index = 10; index <= 100; index= index+10) {
+    console.log(index);
+    
+}
+console.log(`============Step 07=============`);
+
+var result = "";
+for (let index = 100; index >= 10; index=index-10) {
+result = result + index + " ";
+   console.log(index);
+    
+}
+console.log(result);
