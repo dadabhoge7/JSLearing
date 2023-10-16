1b8394feb7c6f4a478bcceacb490da819b0b90f8
dadabhoge7 <dadabhoge7@gmail.com>
2023-10-16 10:37:43 +0530

    add 06_primeNumAssignment
    add 06_primeNumAssignment

---
+++ 06_primeNumAssign.js
@@ -0,0 +1,21 @@
+
+
+
+const array = [11,3,6,10,43,29,50];
+
+for (let index = 0; index < array.length; index++) {
+    
+    const element = array[index];
+    
+    function isPrimeNum(element){
+        for (let index = 2; index < element; index++) {
+            if (element%index==0) {
+                return false
+            }
+        }
+        return true;
+    }
+    const result = isPrimeNum(element);
+    console.log(`"${element}" Is Prime Number : ${result}`);
+
+}
