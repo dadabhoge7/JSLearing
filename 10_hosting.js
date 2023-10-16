19321ba48fe0388a4ca4c3bec276b8b9c2d1f58c
dadabhoge7 <dadabhoge7@gmail.com>
2023-10-16 10:40:20 +0530

    10_hosting
    10_hosting

---
+++ 10_hosting.js
@@ -0,0 +1,37 @@
+
+console.log(`-----> Variable Hosting <-------`);
+console.log(personName);
+// console.log(age);
+var personName = "elonMusk";
+let age = 22;
+console.log(age);
+console.log(personName);
+
+
+console.log(`------> Function Hosting <------`);
+
+add();
+function add() {
+    console.log(`Addition : ${10+10}`);
+}
+
+multiply();
+const multiply = function() {
+    console.log(`multiply : ${10*10}`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
