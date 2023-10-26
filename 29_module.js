f295c0f3d5881eb4d838f0ff85c7383f6885f6ca
dadabhoge7 <dadabhoge7@gmail.com>
2023-10-26 11:15:13 +0530

    29_module
    29_module

---
+++ 29_module.js
@@ -0,0 +1,9 @@
+const PI = 3.14;
+
+export default PI;//default function
+
+export const arrayNum = [11,22,33,44,55,66,7,88,99];// named exportt 
+
+export const sum = (n1, n2) =>{
+console.log(`sum of ${n1} and ${n2} is : ${n1 + n2}`);
+}
